57c40aa3391edcb487118fe905cf35a22ed9e6e3
cristofer.pereira <cristofer.pereira@shx.com.br>
2015-11-27 10:56:16 -0200

    v1 website opensanca

---
+++ base/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer -->
+<footer>
+    <div class="container text-center">
+        <p>Feito com <i class="fa fa-heart"></i> opensanca 2015</p>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="js/jquery.easing.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/custom.js"></script>
+<script async defer src="http://slack-opensanca.herokuapp.com/slackin.js"></script>
+
+
+</body>
+
+</html>
