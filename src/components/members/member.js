7763229798402b513eb5ef3ac8c23615af4db869
marcel0ll <marcelo@lotuz.dev>
2020-11-22 16:15:05 -0300

    Atualize membros

--- src/components/members/member.js
+++ src/components/members/member.js
@@ -2,23 +2,19 @@ import React from "react"
 
 const Member = ({ img = null, member }) => {
   return (
-    <div className="col-sm-4">
+    <a 
+      style={{color: "white"}}
+      className="col-sm-4"
+          href={member.url}
+          target="_blank"
+          rel="noreferrer"
+     >
       {img}
       <h5>
         {member.name}
         {member.company ? ` @${member.company}` : ""}
       </h5>
-      <span className="input-group-btn">
-        <a
-          href={member.url}
-          target="_blank"
-          rel="noreferrer"
-          className="btn btn-default btn-green"
-        >
-          Who?
-        </a>
-      </span>
-    </div>
+    </a>
   )
 }
 
