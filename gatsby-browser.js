e4f6812c4b30a46d30ac734b9df9e53b87b676b9
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-11-23 21:33:51 -0300

    Instale o smooth scroll para rolagem

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+export const wrapPageElement = ({ element }) => {
+  if (typeof window !== "undefined") {
+    // eslint-disable-next-line global-require
+    require("smooth-scroll")('a[href*="#"]');
+  }
+  return element;
+};
