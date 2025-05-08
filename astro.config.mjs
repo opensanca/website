1663711ac688ca0e1faa632b6bea44c20c527f97
marcel0ll <marcelo@lotuz.dev>
2025-05-08 15:38:18 -0300

    config for github actions deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://opensanca.com.br',
   integrations: [mdx(), sitemap()],
 
   vite: {
