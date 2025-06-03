dde3c82eb328b3c5f89e172aa2eedbaa1c9f395c
marcel0ll <marcelo@lotuz.dev>
2025-06-02 22:54:51 -0300

    make blog/events work

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string())
 	}),
 });
 
