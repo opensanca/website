40349c6e37b0cf3554e77b154f1c3b6235f27e68
marcel0ll <marcelo@lotuz.dev>
2025-06-24 18:18:57 -0300

    Adjust text: Open Source, Opensanca

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Website
 
 Website para divulgar eventos, workshops entre tantas outras sobre o mundo
-opensource e a vida ativa de TI em sancharles city!
+Open Source e a vida ativa de TI em sancharles city!
 
 ## Tecnologia
 
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Opensanca';
-export const SITE_DESCRIPTION = 'opensanca | Meetup Sancarlense sobre opensource, startups e beer code.';
+export const SITE_DESCRIPTION = 'Opensanca | Meetup Sancarlense sobre Open Source, startups e beer code.';
